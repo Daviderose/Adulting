78945ff6af897ea44f1bcc33260c1e23a746e1ee
daviderose526 <daviderose526@gmail.com>
2018-08-15 14:58:53 -0600

    Added additional comments

--- src/main/java/org/launchcode/Adulting/hashing/SHA256Hashing.java
+++ src/main/java/org/launchcode/Adulting/hashing/SHA256Hashing.java
@@ -6,6 +6,7 @@ import java.nio.charset.StandardCharsets;
 
 public class SHA256Hashing {
 
+    // class for SHA256 hashing
     public static String HashWithGuava(final String originalString) {
         final String sha256hex = Hashing.sha256().hashString(originalString, StandardCharsets.UTF_8).toString();
         return sha256hex;
